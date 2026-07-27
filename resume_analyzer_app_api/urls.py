385168d42528adac905780fad07edb0bdfe93de4
Jorge Devops <bk11iscool@gmail.com>
2026-07-26 23:18:34 -0700

    Resume Analyzer API urls.py cleanup

--- resume_analyzer_app_api/urls.py
+++ resume_analyzer_app_api/urls.py
@@ -1,27 +1,3 @@
-# from django.urls import path, include
-# from rest_framework.routers import DefaultRouter
-# from .views import UserViewSet
-
-# router = DefaultRouter()
-# router.register(r'user', UserViewSet)
-
-# urlpatterns = [
-#     path("", include(router.urls)),
-# ]
-
-# from django.urls import path, include 
-# # from rest_framework.routers import DefaultRouter 
-# # from .views import UserViewSet 
-
-# # router = DefaultRouter() 
-# # router.register(r"user", UserViewSet) 
-
-
-# urlpatterns = [ 
-#     path( "auth/", include("resume_analyzer_app_api.auth_urls") ),
-#     path( "", include(router.urls) ), 
-# ]
-
 from django.urls import path, include
 from .views import CurrentUserView
 
