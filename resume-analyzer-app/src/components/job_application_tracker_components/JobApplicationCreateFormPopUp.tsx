b1d53b3c26e8e064ec908439295225dc2498dee1
Jorge Devops <bk11iscool@gmail.com>
2026-08-23 02:35:38 -0700

    Resume Analyzer JobApplicationCreateFormPopUp.tsx Functionaly Complete: - Will include more functions later. - Updates to TS / HTML / CSS throughout. - Also Changed the positioning of the pop-ups from absolute to fixed (Viewport based positioning rather than page-start based positioning).

--- resume-analyzer-app/src/components/job_application_tracker_components/JobApplicationCreateFormPopUp.tsx
+++ resume-analyzer-app/src/components/job_application_tracker_components/JobApplicationCreateFormPopUp.tsx
@@ -1,4 +1,6 @@
+import {useState} from 'react'
 import "../../css/job_application_tracker/JobApplicationCreateFormPopUp.css"
+import {createJobApplication} from "../../api/job_application_service"
 
 
 export default function JobApplicationCreateFormPopUp(
@@ -7,15 +9,40 @@ export default function JobApplicationCreateFormPopUp(
     }:{
         handleSetCreateFormIsVisible: (state: boolean) => void        
     }
-){
+){  
+
+    const [companyName, setCompanyName] = useState("");
+    const [jobTitle, setJobTitle] = useState("");
+
+
+
+    function onSubmit(){
+        createJobApplication({company_name: companyName, job_title: jobTitle})
+    }
 
     return(
         <div className="JobApplicationCreateFormPopUpWrapper">
-            <div onClick={()=> handleSetCreateFormIsVisible(false)}>Exit</div>    
-            <form className="JobApplicationCreateForm">
-                <input/>
-                <input/>
-                <input/>
+            <div onClick={()=> handleSetCreateFormIsVisible(false)}>Exit</div>
+                
+            <form className="JobApplicationCreateForm" onSubmit={()=>onSubmit()}>      
+                <h1 className="JobApplicationCreateFormHeadingText">Add Job</h1>  
+                <input 
+                className="JobApplicationCreateFormInput"
+                value={companyName}
+                onChange={
+                    e => setCompanyName(e.target.value)
+                }
+                placeholder="Company Name"
+                />
+                <input
+                className="JobApplicationCreateFormInput"
+                value={jobTitle}
+                onChange={
+                    e => setJobTitle(e.target.value)
+                } 
+                placeholder="Job Title"
+                />
+                <button className="JobApplicationCreateFormSubmitButton" type="submit">Create</button>
             </form>
         </div>
     )
