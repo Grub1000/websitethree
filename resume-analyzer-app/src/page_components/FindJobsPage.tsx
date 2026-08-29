0ad574fa61db3566f0294f1a56e01227b08a3a70
Jorge Devops <bk11iscool@gmail.com>
2026-08-29 05:58:31 -0700

    Resume Analyzer Added a Coming Soon Section To The Find Jobs Page.

--- resume-analyzer-app/src/page_components/FindJobsPage.tsx
+++ resume-analyzer-app/src/page_components/FindJobsPage.tsx
@@ -3,6 +3,26 @@ import "../css/resume_analyzer_css/FindJobsPage.css"
 
 export default function FindJobsPage(){
     return(
-        <div>Find Jobs Page Coming Soon!</div>
+    <div className="container">
+        <div className="content">
+            <span className="badge">Exciting Updates</span>
+            <h1>Find Jobs Feature Coming Soon</h1>
+            <p>Website is under construction. I'm are working hard to bring you an amazing new experience.</p>
+            {/* <div className="countdown">
+                <div className="time-box">
+                    <span className="number">45</span>
+                    <span className="label">Days</span>
+                </div>
+                <div className="time-box">
+                    <span className="number">12</span>
+                    <span className="label">Hours</span>
+                </div>
+                <div className="time-box">
+                    <span className="number">30</span>
+                    <span className="label">Minutes</span>
+                </div>
+            </div> */}
+        </div>
+    </div>
     )
 }
